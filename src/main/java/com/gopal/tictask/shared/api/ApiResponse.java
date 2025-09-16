aeacaf3d5c3a7eaf89c1a84cc747274846f95d5a
GopalShivhare <gopalshivhare144@gmail.com>
2025-09-16 14:45:16 +0530

    refactor: minor refactor in User and Api response

--- src/main/java/com/gopal/tictask/shared/api/ApiResponse.java
+++ src/main/java/com/gopal/tictask/shared/api/ApiResponse.java
@@ -2,20 +2,21 @@ package com.gopal.tictask.shared.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ApiResponse<T> {
 
     private boolean success;
     private String message;
     private T data;
-
-    public ApiResponse() {}
-
-    public ApiResponse(boolean success, String message, T data) {
-        this.success = success;
-        this.message = message;
-        this.data = data;
-    }
 
     public static <T> ApiResponse<T> success(String message, T data) {
         return new ApiResponse<>(true, message, data);
@@ -28,14 +29,5 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> failure(String message) {
         return new ApiResponse<>(false, message, null);
     }
-
-     public boolean isSuccess() { return success; }
-     public void setSuccess(boolean success) { this.success = success; }
- 
-     public String getMessage() { return message; }
-     public void setMessage(String message) { this.message = message; }
- 
-     public T getData() { return data; }
-     public void setData(T data) { this.data = data; }
 
 }
